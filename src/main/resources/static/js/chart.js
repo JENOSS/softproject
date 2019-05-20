7d884643b2d2894c7e7c2d19d5ee251f272cf38f
integer <dooswan@gmail.com>
2019-05-20 17:12:32 +0900

    integer edit chart.js

--- src/main/resources/static/js/chart.js
+++ src/main/resources/static/js/chart.js
@@ -80,7 +80,8 @@ $(function() {
   };
   var doughnutPieData = {
     datasets: [{
-      data: [30, 40, 30],
+      // data: [30, 40, 30],
+      data: [40, 60],
       backgroundColor: [
         'rgba(255, 99, 132, 0.5)',
         'rgba(54, 162, 235, 0.5)',
@@ -101,9 +102,9 @@ $(function() {
 
     // These labels appear in the legend and in the tooltips when hovering different arcs
     labels: [
-      'Pink',
-      'Blue',
-      'Yellow',
+      'To-do',
+      'Done',
+      // 'Yellow',
     ]
   };
   var doughnutPieOptions = {
