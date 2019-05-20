96bfe499221b83015132fdc2c9c22de3bd5149d3
HONG1 <sunshine7513@naver.com>
2019-05-20 22:13:27 +0900

    controller push

---
+++ src/main/java/com/park/webservice/controller/TestController.java
@@ -0,0 +1,69 @@
+package com.park.webservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/")
+    public String start(Model model) {
+        //model.addAttribute("userForm", new User());
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        //model.addAttribute("userForm", new User());
+        return "login";
+    }
+
+    @GetMapping("/find_id")
+    public String find_id(Model model) {
+        //model.addAttribute("userForm", new User());
+        return "find_id";
+    }
+
+    @GetMapping("/find_password")
+    public String find_password(Model model) {
+        //model.addAttribute("userForm", new User());
+        return "find_password";
+    }
+
+    @GetMapping("/notice")
+    public String notice(Model model) {
+        //model.addAttribute("userForm", new User());
+        return "notice";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        //model.addAttribute("userForm", new User());
+        return "register";
+    }
+
+    @GetMapping("/schedule")
+    public String schedule(Model model) {
+        //model.addAttribute("userForm", new User());
+        return "schedule";
+    }
+
+    @GetMapping("/storage")
+    public String storage(Model model) {
+        //model.addAttribute("userForm", new User());
+        return "storage";
+    }
+
+    @GetMapping("/teamchat")
+    public String teamchat(Model model) {
+        //model.addAttribute("userForm", new User());
+        return "teamchat";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(Model model) {
+        //model.addAttribute("userForm", new User());
+        return "welcome";
+    }
+}
